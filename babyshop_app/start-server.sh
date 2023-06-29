b535229fbd9e6cf9f59814ae210d9b819edb47fe
lowryel <eugeneagbaglo@gmail.com>
2023-06-29 07:06:30 +0000

    fixed url path in template

--- babyshop_app/start-server.sh
+++ babyshop_app/start-server.sh
@@ -2,7 +2,7 @@
 set -x
 if ( ls | grep "db.sqlite3" );
 then
-	python3 manage.py runserver 0.0.0.0:8000
+	python3 manage.py runserver 0.0.0.0:5000
 else
 	python3 manage.py makemigrations
 	python3 manage.py migrate
